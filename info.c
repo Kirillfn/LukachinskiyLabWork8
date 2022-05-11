a1bab9968ac78a71ba577fe5837e73bf560034db
Kirillfn <kirill67561@gmail.com>
2022-05-11 21:04:08 +0300

    Created by Local Git

---
+++ info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)  {
+        fprintf(stderr, "It is process!\n");
+        fprintf(stderr, "Process group id: %d\n", getpgrp());
+        fprintf(stderr, "Process PID: %d\n", getpid());
+        fprintf(stderr, "Process PPID: %d\n", getppid());
+        fprintf(stderr, "Process user id: %d\n", getuid());
+        fprintf(stderr, "Process user`s group id: %d\n", getgid());
+return 0;
+}					
+		
---
+++ zombie.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void)
+{
+        pid_t pid;
+        pid = fork();
+        if(pid == 0)
+        {
+               printf("I am Zombie-process of Lukachinskiy\n");
+               _exit(0);
+        }
+        else 
+               sleep(10);
+        return 0;
+}
